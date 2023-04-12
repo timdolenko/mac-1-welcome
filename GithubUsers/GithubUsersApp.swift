b356b889d3f146a1961ec472fbcf34b3599c4839
Tim <tymofii.dolenko@axelspringer.com>
2023-04-12 14:10:30 +0200

    Initial Commit

---
+++ GithubUsers/GithubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUsersApp.swift
+//  GithubUsers
+//
+//  Created by tdolenko on 12.04.23.
+//
+
+import SwiftUI
+
+@main
+struct GithubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
